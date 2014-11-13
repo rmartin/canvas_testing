e7d0b7e7671c2618dfe6504d6a65da6c04327965
bryan huizi <bryan.huizi@modop.com>
2014-11-13 15:07:19 -0800

    [ animation | update ]

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,74 +1,80 @@
-(function(){
+(function() {
 	'use strict';
+	var frame = [
+		[75, 300],
+		[200, 200],
+		[325, 625],
+		[450, 550]
+	];
+	var positions = [],
+		i, canvas, ctx, width, height, bounce = -1,
+		point, radius = 50;
 
-	window.requestAnimFrame = (function(callback) {
-			return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-				function(callback) {
-					window.setTimeout(callback, 1000 / 60);
-				};
-		})();
+	canvas = document.getElementById('canvas-container');
+	width = canvas.width;
+	height = canvas.height;
+	ctx = canvas.getContext('2d');
 
-	var canvas = document.getElementById('canvas-container'),
-		ctx = canvas.getContext('2d'),
-		// get window dimensions
-		W = window.innerWidth,
-		H = window.innerHeight;
+	for (i = 0; i < frame.length; i += 1) {
+		positions.push({
+			x: frame[i][0],
+			y: frame[i][1],
+			vx: 10,
+			vy: 0
+		});
+	}
 
-	//set canvas dimensions to window dimensions
-	canvas.width = W;
-	canvas.height = H;
+	function update() {
+		var i, point;
+		for (i = 0; i < frame.length; i += 1) {
+			point = positions[i];
+			point.x += point.vx;
+			point.y += point.vy;
 
-	var requestId;
-
-	var frame = [[75, 100], [200, 100], [325, 100], [450, 100], [525, 100]],
-		words = ['hello01', 'hello02', 'hello03', 'hello04' ],
-		posX = 0,
-		radius = 50,
-		pixelsPerFrame = 5,
-		text;
-
-    function draw(posX, posY){ 
-		for(var i = 0; i < frame.length; i++){
-			ctx.beginPath();
-			ctx.moveTo(posX, 0);
-			ctx.lineTo(frame[i][0], 200);
-			ctx.lineWidth = 2;
-			ctx.lineCap = 'round';
-			ctx.strokeStyle = '#fff';
-			ctx.stroke();
-
-			ctx.beginPath();
-			ctx.arc(frame[i][0], 200, radius, 0, 2 * Math.PI, false);
-			ctx.lineWidth = 2;
-			ctx.strokeStyle = '#fff';
-			ctx.stroke();
-
-			text = words[0];
-			ctx.font = 'italic 20px Calibri';
-			ctx.textAlign = 'center';
-			ctx.textBasline = 'middle';
-			ctx.fillStyle = '#fff';
-			ctx.fillText(text, frame[i][0], 200);
+			if (point.x > width) {
+				point.x = width;
+				point.vx *= bounce;
+			} else if (point.x < 0) {
+				point.x = 0;
+				point.vx *= bounce;
+			}
+			if (point.y > height) {
+				point.y = height;
+				point.vy *= bounce;
+			} else if (point.y < 0) {
+				point.y = 0;
+				point.vy *= bounce;
+			}
 		}
 	}
 
-	function animate(){
-		requestId = requestAnimationFrame(animate);
-		// get last x position of the array
-		var last = frame[frame.length - 1],
-			upperBound = last[0];
-		if(posX <= upperBound) {
-			//clear canvas and increment x position
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			posX += pixelsPerFrame;
-			draw(posX);								
-		} else {
-			// reset position back to zero to create an infinite loop
-			posX = 0;
-			draw(posX);
+	function draw() {
+		ctx.clearRect(0, 0, width, height);
+		ctx.lineWidth = 1.5;
+		ctx.strokeStyle = 'rgb(218, 216, 185)';
+		var i, point;
+		for (i = 0; i < frame.length; i += 1) {
+			point = positions[i];
+			ctx.beginPath();
+			ctx.moveTo(0, 0);
+			ctx.lineTo(point.x, point.y);
+			ctx.stroke();
+
+			// circle
+			ctx.beginPath();
+			ctx.arc(point.x, point.y, radius, 0, 2 * Math.PI, false);
+			//if fillstyle is set to transparent text cannot be added to the circle
+			//if no fillStyle is specified then 
+			//ctx.fillStyle = 'transparent';
+			//ctx.fill();
+			ctx.lineWidth = 2;
+			ctx.strokeStyle = 'rgb(218, 216, 185)';
+			ctx.stroke();
 		}
 	}
 
-
-	requestAnimationFrame(animate);	
+	setInterval(function() {
+		update();
+		draw();
+	}, 1000 / 24);
 })();
