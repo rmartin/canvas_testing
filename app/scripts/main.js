f30ae27790a43c576f833a0023977c68fc3d29c1
bryan huizi <bryan.huizi@modop.com>
2014-11-12 16:34:31 -0800

    [ working animationFram | cancelAnimationFrmae ]

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,5 @@
 (function(){
+	var self = this;
 	'use strict';
 
 	window.requestAnimFrame = (function(callback) {
@@ -11,62 +12,60 @@
 	var canvas = document.getElementById('myCanvas'),
 		canvasW = canvas.width,
 		canvasH = canvas.height,
-		context = canvas.getContext('2d');
+		ctx = canvas.getContext('2d');
 
-	var radius = 50,
-		text = '',
-        words = ['hello01', 'hello02', 'hello03', 'hello04' ],
-        array = [[75, 100], [200, 100], [325, 100], [450, 100]];
+    var requestId;
 
-	function drawData() {
-		context.save();
-		context.clearRect(0,0, canvas.width, canvas.height);
-		var startX;
+    var words = ['hello01', 'hello02', 'hello03', 'hello04' ];
+    var posX = 0;
+    var boxWidth = 50;
+    var radius = 50;
+    var pixelsPerFrame = 5;
 
-		for(var k = 0; k < array.length; k++){
-			startX = array[k][0];
+    ctx.fillStyle = '#212121';
+    ctx.fillRect(posX, 0, canvasH, canvasH);
+
+    function draw(posX){
+    	for(var i = 0; i < words.length; i++){
+				ctx.beginPath();
+				ctx.moveTo(posX, 0);
+				ctx.lineTo(200, 200);
+				ctx.lineWidth = 2;
+				ctx.lineCap = 'round';
+				ctx.strokeStyle = '#fff';
+				ctx.stroke();
+
+				ctx.beginPath();
+				ctx.arc(posX, 0, radius, 0, 2 * Math.PI, false);
+				ctx.lineWidth = 2;
+				ctx.strokeStyle = '#fff';
+				ctx.stroke();
+
+				text = words[0];
+				ctx.font = 'italic 20px Calibri';
+				ctx.textAlign = 'center';
+				ctx.textBasline = 'middle';
+				ctx.fillStyle = '#fff';
+				ctx.fillText(text, posX, 0);
+    			}
+   }
+
+	function animate(){
+		requestId = requestAnimationFrame(animate);
+
+		if(posX <= (canvas.width - boxWidth)) {
+			
+			ctx.clearRect(0, 0, canvas.width, canvas.height);
+			// ctx.fillRect(posX, 0, boxWidth, canvas.height);
+			posX += pixelsPerFrame;
+			draw(posX);
+			
+		} else {
+			cancelAnimationFrame(requestId);
 		}
-		for(var i = 0; i < array.length; i++){
-			// draw lines
-			context.beginPath();
-			context.moveTo(startX, 0);
-			context.lineTo(array[i][0], array[i][1]);
-			context.lineWidth = 2;
-			context.lineCap = 'round';
-			context.strokeStyle = '#fff';
-			context.stroke();
-
-			// circle
-			context.beginPath();
-			context.arc(array[i][0] += 6, array[i][1] += radius, radius, 0, 2 * Math.PI, false);
-			context.lineWidth = 2;
-			context.strokeStyle = '#fff';
-			context.stroke();
-
-			//draw text
-			text = words[i];
-			context.font = 'italic 20px Calibri';
-			context.textAlign = 'center';
-			context.textBasline = 'middle';
-			context.fillStyle = '#fff';
-			context.fillText(text, array[i][0], array[i][1]);
-
-			//get metrics
-			var metrics = context.measureText(text),
-				width = metrics.width;
-			context.font = 'italic 20px Calibri';
-			context.textAlign = 'center';
-			context.fillStyle = '#fff';
-			context.fillText(width, array[i][0], array[i][1] + 20);
-
-
-		}
-		 requestAnimationFrame(drawData);
 	}
 
-	requestAnimationFrame(drawData);
 
-
-	
+requestAnimationFrame(animate);	
 	
 })();
